56aaa820c25bec0b976e5d78eef80a3cadd6dc20
Jonah <jonahwert07@gmail.com>
2026-02-28 15:42:29 -0500

    change sql

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,14 +4,13 @@
 public class Main {
 
     public static void main(String[] args){
-        Customer = new John(1, "John", "Doe", 20, "john@example.com", 9876543211););
+        Customer = new John(1, "John", "Doe", 20, "john@example.com", 9876543211);
         Customer2 = new Jonah(2, "Jonah", "Wert", 22, "jwert@gmail.com", 1234567899);
-        Customer3 = new Jane(3, "Jane", "Doe", 21, "jdoe@gmail.com", 3216549877););
+        Customer3 = new Jane(3, "Jane", "Doe", 21, "jdoe@gmail.com", 3216549877);
 
         System.out.println("John " + Customer.getfirstName());
         System.out.println("Jonah " + Customer2.getfirstName());
         System.out.println("Jane " + Customer3.getfirstName());
-
 
         Customer.firstName();
         Customer2.firstName();
