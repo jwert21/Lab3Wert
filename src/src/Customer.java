f9fcd686630c3b22bf77d1458751f013923bc00e
Jonah <jonahwert07@gmail.com>
2026-02-28 16:01:58 -0500

    change sql

---
+++ src/src/Customer.java
@@ -0,0 +1,36 @@
+public class Customer {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String email;
+    private String phoneNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge()  { return age; }
+
+    public void setAge(String age) { this.age = age; }
+}
