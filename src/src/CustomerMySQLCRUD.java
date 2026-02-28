1d48009cc57ace3ef984540242ffccd66f381889
Jonah <jonahwert07@gmail.com>
2026-02-28 17:13:26 -0500

    change sql

--- src/src/CustomerMySQLCRUD.java
+++ src/src/CustomerMySQLCRUD.java
@@ -21,7 +21,7 @@ public class CustomerMySQLCRUD {
 
         try {
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Store", "root", "IST888IST888");
-            insertCustomer(connection, 1, "John", "Doe", 20, "john@example.com", 9876543211, "123 Pine Street, Philadelphia PA, 19116");
+            insertCustomer(connection, 1, "John", "Doe", 20, "john@example.com", 9876543211, "123 Pine Street, Willow Grove PA, 19090");
 
             for(Customer customer : getAllCustomers(connection)) {
                 System.out.println(customer.toString());
@@ -70,7 +70,7 @@ public class CustomerMySQLCRUD {
             }
 
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Store", "root", "IST888IST888");
-            insertCustomer(connection, 3, "Jane", "Doe", 21, "jdoe@gmail.com", 3216549877, "221 Old York Road, Abington PA, 19001");
+            insertCustomer(connection, 3, "Jane", "Doe", 21, "jdoe@gmail.com", 3216549877, "221 Woodland Road, Abington PA, 19001");
 
             for(Customer customer : getAllCustomers(connection)) {
                 System.out.println(customer.toString());
