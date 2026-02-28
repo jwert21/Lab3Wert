07c43f738d5af9ddcd11f9174b112181f63bab9d
Jonah <jonahwert07@gmail.com>
2026-02-28 17:10:59 -0500

    change sql

--- src/src/CustomerMySQLCRUD.java
+++ src/src/CustomerMySQLCRUD.java
@@ -21,7 +21,7 @@ public class CustomerMySQLCRUD {
 
         try {
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Store", "root", "IST888IST888");
-            insertCustomer(connection, 1, "John", "Doe", 20, "john@example.com", 9876543211);
+            insertCustomer(connection, 1, "John", "Doe", 20, "john@example.com", 9876543211, "123 Pine Street, Philadelphia PA, 19116");
 
             for(Customer customer : getAllCustomers(connection)) {
                 System.out.println(customer.toString());
@@ -45,7 +45,7 @@ public class CustomerMySQLCRUD {
                 e.printStackTrace();
             }
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Store", "root", "IST888IST888");
-            insertCustomer(connection, 2, "Jonah", "Wert", 22, "jwert@gmail.com", 1234567899);
+            insertCustomer(connection, 2, "Jonah", "Wert", 22, "jwert@gmail.com", 1234567899, "456 Broad Street, Philadelphia PA, 19115");
 
             for(Customer customer : getAllCustomers(connection)) {
                 System.out.println(customer.toString());
@@ -70,7 +70,7 @@ public class CustomerMySQLCRUD {
             }
 
             connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/Store", "root", "IST888IST888");
-            insertCustomer(connection, 3, "Jane", "Doe", 21, "jdoe@gmail.com", 3216549877);
+            insertCustomer(connection, 3, "Jane", "Doe", 21, "jdoe@gmail.com", 3216549877, "221 Old York Road, Abington PA, 19001");
 
             for(Customer customer : getAllCustomers(connection)) {
                 System.out.println(customer.toString());
@@ -98,8 +98,8 @@ public class CustomerMySQLCRUD {
 
     }
 
-    private static void insertCustomer(Connection connection, int id, String firstName, String lastName, int age, String email, int phoneNumber) throws SQLException {
-        String sql = "INSERT INTO customers (id, firstName, lastName, age, email, phoneNumber) VALUES (?, ?, ?, ?, ?, ?)";
+    private static void insertCustomer(Connection connection, int id, String firstName, String lastName, int age, String email, int phoneNumber, String address) throws SQLException {
+        String sql = "INSERT INTO customers (id, firstName, lastName, age, email, phoneNumber, address) VALUES (?, ?, ?, ?, ?, ?)";
 
         try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setInt(1, id);
@@ -107,7 +107,8 @@ public class CustomerMySQLCRUD {
             preparedStatement.setString(3, lastName);
             preparedStatement.setInt(4, age);
             preparedStatement.setString(5, email);
-            preparedStatement.setString(6, phoneNumber)
+            preparedStatement.setString(6, phoneNumber);
+            preparedStatement.setString(7, address);
             preparedStatement.executeUpdate();
         }
 
@@ -115,7 +116,7 @@ public class CustomerMySQLCRUD {
 
     private static List<Customer> getAllCustomers(Connection connection) throws SQLException {
         List<Customer> customers = new ArrayList();
-        String sql = "SELECT id, firstName, lastName, age, email, phoneNumber FROM customers";
+        String sql = "SELECT id, firstName, lastName, age, email, phoneNumber, address FROM customers";
 
         try (
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
