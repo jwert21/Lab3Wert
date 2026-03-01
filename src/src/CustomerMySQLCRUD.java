58e19db1de7733ee2335ccf5579e3bd701806114
Jonah <jonahwert07@gmail.com>
2026-03-01 17:17:29 -0500

    change sql

--- src/src/CustomerMySQLCRUD.java
+++ src/src/CustomerMySQLCRUD.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerMySQLCRUD {
-    private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/Store"
+    private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/Store";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "IST888IST888";
 
