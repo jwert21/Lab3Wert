4306116328141ed7d013fb1a4fb8b9dcb3734c25
Jonah <jonahwert07@gmail.com>
2026-03-01 17:44:40 -0500

    change sql

--- src/src/Customer.java
+++ src/src/Customer.java
@@ -1,3 +1,13 @@
+/** Project: Lab 3
+ * Purpose Details:
+ * Course: IST 242
+ * Author: Jonah Wert
+ * Date Developed: 2/26/2026
+ * Last Date Changed: 3/1/2026
+ * Rev: 3/1/2026
+
+ */
+
 public class Customer {
     private String id;
     private String firstName;
