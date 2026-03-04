c274c2d8f1c8de24a1e6fb602c29af6e95f40b81
Jonah <jonahwert07@gmail.com>
2026-03-03 21:35:27 -0500

    Merge remote-tracking branch 'origin/main'

--- src/src/Main.java
+++ src/src/Main.java
@@ -14,22 +14,22 @@ public class Main {
         Customer customer2 = new Customer(2, "Jonah", "Wert", 22, "jwert@gmail.com", "1234569877");
         Customer customer3 = new Customer(3, "Jane", "Doe", 21, "jdoe@gmail.com", "3216549877");
 
-        StudentCRUDExample mysql = new StudentCRUDExample();
-        StudentMongoCRUDExample mongo = new StudentMongoCRUDExample();
+        CustomerCRUDExample mysql = new CustomerCRUDExample();
+        CustomerMongoCRUDExample mongo = new CustomerMongoCRUDExample();
 
         // CREATE
-        mysql.create(student1);
-        mongo.create(student1);
+        mysql.create(customer1);
+        mongo.create(customer1);
 
         // READ (read the same id you created)
         mysql.read(1);
         mongo.read(1);
 
         // UPDATE
-        student1.setEmail("newjohn@example.com");
-        student1.setFirstName("Johnny");
-        mysql.update(student1);
-        mongo.update(student1);
+        customer1.setEmail("newjohn@example.com");
+        customer1.setFirstName("John");
+        mysql.update(customer1);
+        mongo.update(customer1);
 
         // DELETE
         mysql.delete(1);
