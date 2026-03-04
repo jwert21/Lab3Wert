51fdf04b90664d8a253e1af9fa4be43057e51e8a
Jonah <jonahwert07@gmail.com>
2026-03-04 15:31:57 -0500

    Merge remote-tracking branch 'origin/main'

--- src/src/CustomerMySQLCRUD.java
+++ src/src/CustomerMySQLCRUD.java
@@ -9,7 +9,7 @@
  */
 import java.sql.*;
 
-public class CustomerMySQLCRUD implements CustomerCRUD {
+public class CustomerMySQLCRUD {
 
     private static final String URL = "jdbc:mysql://localhost:3306/store";
     private static final String USER = "root";
